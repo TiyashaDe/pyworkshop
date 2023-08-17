6201207481776883044dd36911c9395e230ae0db
tiyasha de <tiyashade2000@gmail.com>
2023-08-17 22:30:08 +0530

    created tuples

---
+++ PythonClass/Tuples.py
@@ -0,0 +1,16 @@
+#a list[]
+#tuple()
+my_tup=()
+type(my_tup)
+my_tup=("hi")
+type(my_tup)
+#tuple(,)
+(1,)
+my_tup=(1,)
+type(my_tup)
+student=("Marcy",8,"History",3.5)
+student[0]
+student
+name,age,subject,gpa=student
+name
+age
