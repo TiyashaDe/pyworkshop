4d13bc95e8b27b59bf559de75fbdb3a22072140a
tiyasha de <tiyashade2000@gmail.com>
2023-08-17 21:24:11 +0530

    commit

---
+++ PythonClass/Datatype,string,numbers.py
@@ -0,0 +1,30 @@
+x=52
+y=3.0
+z=11j
+type(x)
+name=123
+type(name)
+help(str)
+help(str.isupper)
+print("hi")
+name="Riya"
+print(name)
+print(name,5,".")
+f"My name is{name}"
+f"My name is{12/6}"
+name.upper()
+name.lower()
+f"My name is{name.upper()}"
+my_list=["Hello","Hola"]
+my_list.append("Hi!")
+my_list
+my_list[0]
+my_list[0]="What's up"
+my_list
+my_list.pop()
+my_list
+sorted(my_list)
+my_list.sort()
+my_list
+my_list.insert(0,"Good")
+my_list
