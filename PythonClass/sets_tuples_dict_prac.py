52ddf523caea1587c4e71a39c0e27c66b340d202
tiyasha de <tiyashade2000@gmail.com>
2023-08-19 12:28:52 +0530

    sets_tuples_dict_pract.py

---
+++ PythonClass/sets_tuples_dict_prac.py
@@ -0,0 +1,39 @@
+my_set={}
+type(my_set)
+type(set()) #Empty set
+[]  #Empty list
+{}  #Empty dict
+(1,) #Empty tuple-immutable
+set() #Empty set
+hash(set{})
+hash(5)
+hash("hi")
+names=["Nina","Max","Max"]
+set(names)
+set[0]
+my_names_set=set(names)
+my_names_set[0]
+my_tuples=(1,)
+my_tuples
+type(my_tuples)
+my_tuples=1,
+type(my_tuples)
+my_tuples
+student=("john",7,"Math",5.7)
+name,age,subject,gpa=student
+name
+age
+subject
+gpaname,age,subject,_=student
+my_dict={"Key":"Value"}
+my_dict
+my_dict["Key"]
+my_dict["Key"]="a new value"
+my_dict
+"Key" in my_dict
+"not a key" in my_dict
+my_list=[1,2,3]
+my_list[0]=-99
+my_list
+student
+student[0]
