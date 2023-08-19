7e1ba4d4caa4ff4d6f6772cce03f06131307e801
tiyasha de <tiyashade2000@gmail.com>
2023-08-19 11:53:53 +0530

    boolean_logic.py

---
+++ PythonClass/boolean_logic.py
@@ -0,0 +1,52 @@
+3<5
+5>3
+bool(1)
+bool(0)
+bool(3<5)
+bool([])
+bool([1,2,3])
+bool(None)
+1==1
+1 !=1
+list1=[1,2,3]
+list2=[1,2,3]
+list1==list2
+list1 is list2#check for identiy.do these point to the same place in memory?
+True
+False
+bool(None)
+x =None
+x is None
+[] is None
+a= True
+b=True
+True and False
+True and True
+False and False
+True and (True or False)
+not a
+if 3<5:
+    print("Hi!")
+    
+if 5<3:
+    print("Hi!")
+a=True
+b=False
+if not b:
+    print("Hi!")
+    
+if{}:
+    print("Hi!")
+if{1,2}:
+    print("Hi")
+    
+b=[]
+if b:
+    print("Hi")
+if b==True: #Don't do this!
+    print("Hi!") 
+if 5<3:
+    print("Hi")
+else:
+    print("Bye")
+    
