80832e0c83b798a8c26522ddd68d16f3ca4834d2
tiyasha de <tiyashade2000@gmail.com>
2023-08-21 20:26:03 +0530

    name_lib.py

---
+++ PythonClass/other_program.py
@@ -0,0 +1,5 @@
+import name_lib
+my_name="Riya"
+my_length=main_lib.name_length(my_name)
+my_lower_case=main.lib.lower_case_name(my_name)
+print(f"In my code,my length is {my_length} and my lower case name is:{my_lower_case})")
