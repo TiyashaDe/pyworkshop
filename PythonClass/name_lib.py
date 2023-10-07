3686c1aee873a8daafa6261ca0fe18db4e51de7e
tiyasha de <tiyashade2000@gmail.com>
2023-10-07 19:59:20 +0530

    Update name_lib.py

--- PythonClass/name_lib.py
+++ PythonClass/name_lib.py
@@ -6,8 +6,9 @@ def lower_case_name(name):
     return name.lower() 
 def upper_case_name(name):
     return name.upper()
-if __name__=="--main__":
-name="Riya"
-length=name_length(name)
-upper_case=upper_case_name(name)
-print(f"The length is {length} and the uppercase version is:{upper_case}")
+
+if __name__=="__main__":
+  name ="Riya"
+  length=name_length(name)
+  upper_case=upper_case_name(name)
+  print(f"The length is {length} and the uppercase version is:{upper_case}")
